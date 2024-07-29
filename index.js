d3692a7ae3de6edca1feecbb68df09a4b2b91292
Supriya334 <124588951+Supriya334@users.noreply.github.com>
2024-07-29 15:53:12 +0530

    port

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // jai shree ram
 
+document.getElementById('hamburger').addEventListener('click', function () {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
